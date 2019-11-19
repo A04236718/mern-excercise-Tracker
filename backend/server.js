bd9f7ef6bf461aeb6c82f8c62d078de48899857d
Muhammad Ahmed <muhammadahmed@Muhammads-MacBook-Pro-2.local>
2019-11-18 23:27:54 -0600

    Connection/ configuration to database successfully established

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const mongoose = require('mongoose');
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex: true});
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB Connection Successfully established");
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
