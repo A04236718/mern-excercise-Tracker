89f75d273f957c06a0ef2b1d82195346a43d104b
Muhammad Ahmed <muhammadahmed@Muhammads-MacBook-Pro-2.local>
2019-11-25 14:34:53 -0600

    Stub code added to each component to render

---
+++ src/components/create-user.component.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default Class CreateUser extends Component{
+
+    render(){
+        return(
+            <p>You are in the CreateUser Component</p>
+        )
+    }
+}
---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class EditExercise extends Component{
+
+    render(){
+        return(
+            <p>You Are on the Edit Exercise Component</p>
+        )
+    }
+}
